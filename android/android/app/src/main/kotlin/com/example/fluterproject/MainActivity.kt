3b3bbdc321ec5a0703648b38e8cfad9cf3d46e30
khanhkhanh1234 <nguyenkimt322@gmail.com>
2022-11-13 17:47:56 +0700

    second commit

---
+++ android/android/app/src/main/kotlin/com/example/fluterproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluterproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:fluterproject/route/app_page.dart';
+import 'package:fluterproject/route/app_route.dart';
+import 'package:fluterproject/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+     getPages: AppPage.list  ,
+      initialRoute: AppRoute.dashboard,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
